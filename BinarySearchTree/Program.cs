655f28069fd928542e8bb4d0e5b9b651c32b4acf
Biswas <BISWAPR2@novartis.net>
2021-07-19 02:27:26 +0200

    Add project files.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+// https://github.com/kc70x/BinaryTreeExample/blob/master/BinaryTreeExample/Program.cs
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bst = new BinarySearchTree();
+
+            /*
+            bst.Insert(75); //root
+            bst.Insert(57);
+            bst.Insert(90);
+            bst.Insert(32);
+            bst.Insert(7);
+            bst.Insert(44);
+            bst.Insert(60);
+            bst.Insert(86);
+            bst.Insert(93);
+            bst.Insert(99);
+            bst.Insert(100);
+            */
+
+            // Test Data
+            bst.Insert(25); //root
+            bst.Insert(15);
+            bst.Insert(10);
+            bst.Insert(4);
+            bst.Insert(12);
+            bst.Insert(22);
+            bst.Insert(18);
+            bst.Insert(24);
+            bst.Insert(50);
+            bst.Insert(35);
+            bst.Insert(31);
+            bst.Insert(44);
+            bst.Insert(70);
+            bst.Insert(66);
+            bst.Insert(90);
+
+
+            Console.WriteLine("In Order Traversal (Left->Root->Right)");
+            bst.InOrderTraversal();
+
+            Console.WriteLine("\nPre Order Traversal (Root->Left->Right)");
+            bst.PreorderTraversal();
+            
+            Console.WriteLine("\nPost Order Traversal (Left->Right->Root)");
+            bst.PostorderTraversal();
+
+            //
+
+            Console.ReadLine();
+        }
+
+
+        public static int getHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(getHeight(root.LeftNode), getHeight(root.RightNode)) + 1;
+        }
+
+        public static bool isBalanced(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            int heightDiff = getHeight(root.LeftNode) - getHeight(root.RightNode);
+            if (Math.Abs(heightDiff) > 1)
+            {
+                return false;
+            }
+            else
+            {
+                return (isBalanced(root.LeftNode) && isBalanced(root.RightNode));
+            }
+        }
+
+
+    }
+
+
+}
---
+++ SetCursorPosition/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Threading;
+
+// Row,Col representation
+//0,0 0,1 0,2 0,3 0,4
+//1,0             1,4  
+//2,0             2,4 
+//3,0             3,4 
+//4,0 4,1 4,2 4,3 4,4
+//
+//6,0
+
+namespace SetCursorPosition
+{
+    class Program
+    {
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void SetGreenText()
+        {
+             
+            Console.ForegroundColor = ConsoleColor.Green;
+            //Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        private static void SetWhiteText()
+        {
+
+            Console.ForegroundColor = ConsoleColor.White;
+            //Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public static void Main()
+        {
+            // Clear the screen, then save the top and left coordinates.
+            const string stringValue = "Mission Impossible ";
+            // Version 1: use foreach-loop.
+             
+            foreach (char c in stringValue)
+            {
+                 
+                Console.Write(c);
+
+                int rowPos = Console.CursorTop;
+                int colPos = Console.CursorLeft;
+
+                if(colPos >=2)
+                {
+                    int prevCol = colPos - 2;
+                    Console.SetCursorPosition(prevCol, rowPos);
+                    SetGreenText();
+                    char charInString = stringValue[prevCol];
+                    Console.Write(charInString);
+                    Console.SetCursorPosition(colPos  , rowPos);
+                    SetWhiteText();
+                }
+                
+
+
+
+
+                Thread.Sleep(150);
+            }
+
+            Console.WriteLine( );
+            Console.WriteLine("Print any key to continue");
+            Console.ReadLine( );
+
+            Console.Clear();
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+
+            
+
+            // Draw the left side of a 5x5 rectangle, from top to bottom.
+            WriteAt("+", 0, 0);
+            WriteAt("|", 0, 1);
+            WriteAt("|", 0, 2);
+            WriteAt("|", 0, 3);
+            WriteAt("+", 0, 4);
+
+            // Draw the bottom side, from left to right.
+            WriteAt("-", 1, 4); // shortcut: WriteAt("---", 1, 4)
+            WriteAt("-", 2, 4); // ...
+            WriteAt("-", 3, 4); // ...
+            WriteAt("+", 4, 4);
+
+            // Draw the right side, from bottom to top.
+            WriteAt("|", 4, 3);
+            WriteAt("|", 4, 2);
+            WriteAt("|", 4, 1);
+            WriteAt("+", 4, 0);
+
+            // Draw the top side, from right to left.
+            WriteAt("-", 3, 0); // shortcut: WriteAt("---", 1, 0)
+            WriteAt("-", 2, 0); // ...
+            WriteAt("-", 1, 0); // ...
+                                //
+            WriteAt("All done!", 0, 6);
+            Console.WriteLine();
+
+            Console.WriteLine("origRow " + origRow);
+            Console.WriteLine("origCol " + origCol);
+
+            Console.ReadLine();
+        }
+    }
+    /*
+    This example produces the following results:
+
+    +---+
+    |   |
+    |   |
+    |   |
+    +---+
+
+    All done!
+
+    */
+}
